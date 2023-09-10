c06fd6edd2f8e37c4e72b85418358f3b88a06ad8
Willmo3 <thelordofbouge@gmail.com>
2023-09-10 10:20:15 -0400

    Fix tailwind config

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -3,5 +3,15 @@ module.exports = {
   content: [
     "../index.html",
     "../js/*",
-  ]
+  ],
+  theme: {
+    extend: {
+      spacing: {
+        'std-margin': '5vw',
+        'lrg-margin': '10vw'
+      },
+    },
+  },
+  plugins: [],
 }
+
